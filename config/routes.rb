b0a00f97180e7c2f3fca78715af98b065fcaef58
shyam nair <shyamnair1487@gmail.com>
2017-12-06 22:31:25 +0800

    Create new template, action, and route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'about', to: 'pages#about'
+
+resources :articles   #this line gives all the index, create, show, new, edit, destroy, update routes for article(type rails routes)
+
 end
